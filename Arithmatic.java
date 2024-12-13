9e4713596ebf3ca88702e79a9c5c2935aaf224dc
shailesh utkar <106698241+shailws@users.noreply.github.com>
2024-12-13 17:20:37 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,8 @@
+package Day02;
+
+public class Arithmatic {
+  
+public static void main(String[] args) {
+	System.out.println(10+30);
+}
+}
---
+++ Compairision.java
@@ -0,0 +1,7 @@
+package Day02;
+
+public class Compairision {
+   public static void main(String[] args) {
+	System.out.println(10 > 39);
+}
+}
